7c12a0c8ed52acf31e0f7c28ac94b5bc2811c3fc
Knut-Erik Johnsen <abstract@knut-erik.org>
2024-06-26 11:50:56 +0200

    Release/1.15 (#13)
    * Removed extra = in newVersion. Added target to release action to use the correct branch (#7) (#8)
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    * Release/1.14 (#12)
    
    * Removed extra = in newVersion. Added target to release action to use the correct branch (#7)
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    * Feature/update workflows (#11)
    
    * Removed extra = in newVersion. Added target to release action to use the correct branch
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    * Downgraded grpc to same version as the used springboot starter. Added javadoc. Removed componentscan since it's bad practie
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    ---------
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    ---------
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>
    
    ---------
    
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>

--- crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/exception/CrossplaneUnmarshallException.java
+++ crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/exception/CrossplaneUnmarshallException.java
@@ -1,11 +1,23 @@
 package io.crossplane.compositefunctions.starter.exception;
 
+/**
+ * Exception for errors when unmarhsalling
+ */
 public class CrossplaneUnmarshallException extends RuntimeException {
 
+    /**
+     * Constructor with message
+     * @param message The exception message
+     */
     public CrossplaneUnmarshallException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor with message and cause
+     * @param message The exception message
+     * @param cause The throwable that caused the exception
+     */
     public CrossplaneUnmarshallException(String message, Throwable cause) {
         super(message, cause);
     }
--- crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/registration/CrossplaneMetadataBuilder.java
+++ crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/registration/CrossplaneMetadataBuilder.java
@@ -6,24 +6,51 @@ import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Convinience methods to create the Metadata object in any kubernetes resource
+ */
 public class CrossplaneMetadataBuilder {
 
     private CrossplaneMetadataBuilder() {
     }
 
+    /**
+     * Create metadata with name
+     * @param name The name of the resource
+     * @return The metdata object based on the input
+     */
+    public static ObjectMeta createMetadata(String name) {
+        return createMetadata(name, null);
+    }
+
+    /**
+     * Create metadata with name and namespace
+     * @param name The name of the resource
+     * @param namespace The namespace of the resource
+     * @return The metdata object based on the input
+     */
     public static ObjectMeta createMetadata(String name, String namespace) {
         return createMetadata(name, namespace, null);
     }
 
+    /**
+     * Create metadata with name, namespace and annotations
+     * @param name The name of the resource
+     * @param namespace The namespace of the resource
+     * @param annotations Annotations to add to the metadata
+     * @return The metdata object based on the input
+     */
     public static ObjectMeta createMetadata(String name, String namespace, Map<String, String> annotations) {
         return new ObjectMetaBuilder().withName(name).withNamespace(namespace).withAnnotations(annotations).build();
     }
 
 
-    public static ObjectMeta createMetadata(String name) {
-        return createMetadata(name, null);
-    }
-
+    /**
+     * Add extra metadata to a Metadata object
+     * @param annotations The extra metadata to add
+     * @param objectMeta The metadata object to add it to
+     * @return The metadata with the added annotations
+     */
     public static ObjectMeta addAnnotations(Map<String, String> annotations, ObjectMeta objectMeta) {
 
         Map<String, String> existingAnnotations = objectMeta.getAnnotations();
