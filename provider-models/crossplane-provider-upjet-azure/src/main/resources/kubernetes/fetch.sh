a62e73a11b3f7d5ebc07aeafd657ce793bb1801c
Knut-Erik Johnsen <abstract@knut-erik.org>
2024-10-11 13:39:39 +0200

    Fetch the correct version of files, and print if any error fetching the crd
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>

--- provider-models/crossplane-provider-upjet-azure/src/main/resources/kubernetes/fetch.sh
+++ provider-models/crossplane-provider-upjet-azure/src/main/resources/kubernetes/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-release="v1.7.0"
+release="v1.0.1"
 
 if [ $# -eq 1 ]
   then
@@ -9,12 +9,17 @@ fi
 
 echo "Fetching release: ${release}"
 
-crds=$(gh api --jq '.[].name' "/repos/crossplane-contrib/provider-upjet-azure/contents/package/crds/?ref=${release}")
+crds=$(gh api --jq '.[].name' "/repos/crossplane-contrib/provider-upjet-azure/contents/package/crds?ref=${release}")
 
 
 for crd in $crds;
 do
   # output=$(echo $file | cut -d'_' -f2)
   gh api   -H "Accept: application/vnd.github.raw+json"  "/repos/crossplane-contrib/provider-upjet-azure/contents/package/crds/${crd}?ref=${release}" > $crd
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      echo "Failed to fetch ${crd}"
+  fi
+
 done
 
--- provider-models/crossplane-provider-upjet-azuread/src/main/resources/kubernetes/fetch.sh
+++ provider-models/crossplane-provider-upjet-azuread/src/main/resources/kubernetes/fetch.sh
@@ -9,12 +9,16 @@ fi
 
 echo "Fetching release: ${release}"
 
-crds=$(gh api --jq '.[].name' "/repos/crossplane-contrib/provider-upjet-azuread/contents/package/crds/?ref=${release}")
+crds=$(gh api --jq '.[].name' "/repos/crossplane-contrib/provider-upjet-azuread/contents/package/crds?ref=${release}")
 
 
 for crd in $crds;
 do
   # output=$(echo $file | cut -d'_' -f2)
   gh api   -H "Accept: application/vnd.github.raw+json"  "/repos/crossplane-contrib/provider-upjet-azuread/contents/package/crds/${crd}?ref=${release}" > $crd
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      echo "Failed to fetch ${crd}"
+  fi
 done
 
