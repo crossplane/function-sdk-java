dc9f0b54a015ca319a78457ea18090d995eb771d
Knut-Erik Johnsen <knut-erik.johnsen@klp.no>
2025-12-30 00:03:47 +0100

    Fetch and build families and split into smaller pieces
    Signed-off-by: Knut-Erik Johnsen <knut-erik.johnsen@klp.no>

---
+++ provider-models/crossplane-provider-upjet-azure/fetchAndBuild.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+javaRelease="2.3.0"
+release="v${javaRelease}"
+split="false"
+
+if [ $# -eq 2 ]
+then
+  javaRelease="$1"
+  release="v$1"
+  split="$2"
+fi
+
+echo "Fetching release: ${release}"
+
+commitSha=$(gh api "repos/crossplane-contrib/provider-upjet-azure/git/ref/tags/${release}" -q '.object.sha')
+commitTreeSha=$(gh api "repos/crossplane-contrib/provider-upjet-azure/git/commits/${commitSha}" -q '.tree.sha')
+packageTreeSha=$(gh api "repos/crossplane-contrib/provider-upjet-azure/git/trees/${commitTreeSha}" -q '.tree[] | select(.path=="package") | .sha')
+crdsTreeSha=$(gh api "repos/crossplane-contrib/provider-upjet-azure/git/trees/${packageTreeSha}" -q '.tree[] | select(.path=="crds") | .sha')
+
+# echo "Resolved commit SHA: ${commitSha}"
+# echo "Resolved commit tree SHA: ${commitTreeSha}"
+# echo "Resolved package tree SHA: ${packageTreeSha}"
+# echo "Resolved crds tree SHA: ${crdsTreeSha}"
+
+crds=$(gh api "repos/crossplane-contrib/provider-upjet-azure/git/trees/${crdsTreeSha}" --jq '[ .tree[].path]' | jq -r '.[]')
+#crds=$(gh api --jq '.[].name' "/repos/crossplane-contrib/provider-upjet-azure/contents/package/crds?ref=${release}")
+
+groups=$(echo -e "$crds" | cut -d'.' -f1 | sort | uniq)
+
+for crd in $crds;
+do
+  folder="./src/main/resources/kubernetes/"
+  if [ "$split" = "true" ] ; then
+    folder="${folder}$(echo $crd | cut -d'.' -f1 )/"
+    mkdir -p "${folder}"
+  fi
+  # output=$(echo $file | cut -d'_' -f2)
+  gh api   -H "Accept: application/vnd.github.raw+json"  "/repos/crossplane-contrib/provider-upjet-azure/contents/package/crds/${crd}?ref=${release}" > "${folder}${crd}"
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+      echo "Failed to fetch ${crd}"
+  fi
+
+done
+
+if [ "$split" = "true" ] ; then
+  for group in $groups; do
+    mvn -B deploy --file pom.xml -Pdeploy -Dmodelrevision="${javaRelease}" -DmodelDirectory="/${group}" -DmodelGroup="${group}"
+  done
+fi
+mvn -B deploy --file pom.xml -Pdeploy -Dmodelrevision="${javaRelease}"
+
+
+
+
+
+
