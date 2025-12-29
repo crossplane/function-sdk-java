0a82e434bf1b86d0909005a91837f07f2b7c2dde
Knut-Erik Johnsen <knut-erik.johnsen@klp.no>
2025-12-30 00:41:13 +0100

    Hardset variables when building to avoid duplicates
    Signed-off-by: Knut-Erik Johnsen <knut-erik.johnsen@klp.no>

--- provider-models/crossplane-provider-upjet-azure/fetchAndBuild.sh
+++ provider-models/crossplane-provider-upjet-azure/fetchAndBuild.sh
@@ -44,12 +44,16 @@ do
 
 done
 
+mvn versions:set-property -Dproperty=modelrevision "-DnewVersion=${javaRelease}" -DgenerateBackupPoms=false
+mvn -B deploy --file pom.xml -Pdeploy -Dmodelrevision="${javaRelease}"
 if [ "$split" = "true" ] ; then
   for group in $groups; do
+    mvn versions:set-property -Dproperty=modelDirectory -DnewVersion="/${group}" -DgenerateBackupPoms=false
+    mvn versions:set-property -Dproperty=modelGroup -DnewVersion="${group}" -DgenerateBackupPoms=false
     mvn -B deploy --file pom.xml -Pdeploy -Dmodelrevision="${javaRelease}" -DmodelDirectory="/${group}" -DmodelGroup="${group}"
   done
 fi
-mvn -B deploy --file pom.xml -Pdeploy -Dmodelrevision="${javaRelease}"
+
 
 
 
