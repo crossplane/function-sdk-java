c1e8dca790ae0bde000fa276f04e1560eb0a9c56
Knut-Erik Johnsen <knut-erik.johnsen@klp.no>
2026-01-02 16:30:33 +0100

    Add bean for the requiredresources service, and deprecate the extraresources service
    Signed-off-by: Knut-Erik Johnsen <knut-erik.johnsen@klp.no>

--- crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/config/CrossplaneServiceConfiguration.java
+++ crossplane-function-springboot-starter/src/main/java/io/crossplane/compositefunctions/starter/config/CrossplaneServiceConfiguration.java
@@ -2,6 +2,7 @@ package io.crossplane.compositefunctions.starter.config;
 
 import io.crossplane.compositefunctions.starter.conversion.CrossplaneExtraResourcesService;
 import io.crossplane.compositefunctions.starter.conversion.CrossplaneObservableService;
+import io.crossplane.compositefunctions.starter.conversion.CrossplaneRequiredResourcesService;
 import io.crossplane.compositefunctions.starter.conversion.CrossplaneResourceService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,21 @@ public class CrossplaneServiceConfiguration {
      * Set up services for working with extra resources
      * @return the crossplaneExtraResourcesService
      * @since 1.15
+     * @deprecated use {@link CrossplaneRequiredResourcesService} instead
      */
     @Bean
     public CrossplaneExtraResourcesService crossplaneExtraResourcesService() {
         return new CrossplaneExtraResourcesService();
+    }
+
+    /**
+     * Set up services for working with required resources
+     * @return the crossplaneRequiredResourcesService
+     * @since 2.0
+     */
+    @Bean
+    public CrossplaneRequiredResourcesService crossplaneRequiredResourcesService() {
+        return new CrossplaneRequiredResourcesService();
     }
 
     /**
